2988bcd5d1763ca012cb760cacbb5ec444892509
Youssef Achir <43423207+Meihern@users.noreply.github.com>
2021-02-09 21:45:46 +0100

    Updated Kafka Stream Service

---
+++ Kafka-Services/Kafka-Stream-Service/src/main/java/com/tpspringcloud/kafkastreamservice/KafkaStreamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tpspringcloud.kafkastreamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamServiceApplication.class, args);
+    }
+
+}
